d2e12a4f8de867a39e4998ee16f3b3cf060631f1
JJH <jeongjaeho110@gmail.com>
2025-04-27 06:05:46 +0900

    Add files via upload

---
+++ 툰픽/main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 1000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // 점 클릭하면 해당 슬라이드로 이동
+    },
+  });
+  
