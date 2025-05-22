ec3b057a707f121ec51c03dc359aecbcd1107abe
JHLEE0617 <junehlee0617@gmail.com>
2025-05-23 02:10:03 +0900

    데이터베이스 추가

---
+++ 툰픽0.7.3/models/comment.js
@@ -0,0 +1,12 @@
+//models/comments
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  webtoonTitle: String,
+  user: String,
+  content: String,
+  likes: { type: Number, default: 0 },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ 툰픽0.7.3/server.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config();            
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const webtoonRouter = require('./api/webtoon');
+
+app.use(cors());                       
+app.use(express.json());              
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('✅ MongoDB 연결 성공');
+}).catch(err => {
+  console.error('❌ MongoDB 연결 실패:', err);
+});
+
+app.use('/api/comment', require('./api/comment'));
+app.use('/api/preference', require('./api/preference'));
+app.use('/api/webtoon', webtoonRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 서버 실행 중: http://localhost:${PORT}`);
+});
